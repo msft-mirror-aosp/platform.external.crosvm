miyu-xu/external-crosvm

crosvm: fdt: Add option to dump generated devicetree blob
Add an option to dump the crosvm-generated devicetree blob. This option
can be helpful in understanding what devices are reported to the guest
VM and help diagnose issues related to improper devicetree.

Change-Id: Ic6cfe9c12cc01d7aaa66c193b027e1f66d8a02b3
Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

src/crosvm/config.rs
@@ -1193,6 +1193,7 @@ pub struct Config {
     pub display_window_keyboard: bool,
     pub display_window_mouse: bool,
     pub dmi_path: Option<PathBuf>,
+    pub dump_dtb_path: Option<PathBuf>,
     pub enable_hwp: bool,
     pub enable_pnp_data: bool,
     pub executable_path: Option<Executable>,
@@ -1388,6 +1389,7 @@ impl Default for Config {
             display_window_keyboard: false,
             display_window_mouse: false,
             dmi_path: None,
+            dump_dtb_path: None,
             enable_hwp: false,
             enable_pnp_data: false,
             executable_path: None,

src/crosvm/sys/unix.rs
@@ -1718,6 +1718,7 @@ where
         devices,
         irq_chip,
         &mut vcpu_ids,
+        cfg.dump_dtb_path.clone(),
         simple_jail(&cfg.jail_config, "serial_device")?,
         #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         simple_jail(&cfg.jail_config, "block_device")?,

src/sys/windows.rs
@@ -2143,6 +2143,7 @@ where
         pci_devices,
         irq_chip,
         &mut vcpu_ids,
+        cfg.dump_dtb_path.clone(),
         /*debugcon_jail=*/ None,
         None,
     )

msft-mirror-aosp/platform.external.crosvm

Bug: 249043819

src/crosvm/config.rs
@@ -1034,6 +1034,7 @@ pub struct Config {
     pub display_window_keyboard: bool,
     pub display_window_mouse: bool,
     pub dmi_path: Option<PathBuf>,
+    pub dump_device_tree_blob: Option<PathBuf>,
     pub enable_hwp: bool,
     pub enable_pnp_data: bool,
     pub executable_path: Option<Executable>,
@@ -1251,6 +1252,7 @@ impl Default for Config {
             display_window_keyboard: false,
             display_window_mouse: false,
             dmi_path: None,
+            dump_device_tree_blob: None,
             enable_hwp: false,
             enable_pnp_data: false,
             executable_path: None,

src/crosvm/sys/unix.rs
@@ -1840,6 +1840,7 @@ where
         devices,
         irq_chip,
         &mut vcpu_ids,
+        cfg.dump_device_tree_blob.clone(),
         simple_jail(&cfg.jail_config, "serial_device")?,
         #[cfg(any(target_arch = "x86", target_arch = "x86_64"))]
         simple_jail(&cfg.jail_config, "block_device")?,

src/sys/windows.rs
@@ -1962,6 +1962,7 @@ where
         pci_devices,
         irq_chip,
         &mut vcpu_ids,
+        cfg.dump_device_tree_blob.clone(),
         /*debugcon_jail=*/ None,
         None,
     )
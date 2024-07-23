a71fb83ad2630a9c87ea5c29370230f29070a2e5
David Dai <davidai@google.com>
2024-07-23 23:30:51 +0000

    virtcpufreq: Support scaling for custom sized vCPUs
    Allow for custom vCPU capacity and frequency configurations
    by applying a scale to requested and reported frequencies.
    
    This allows users to emulate topologies that are different
    from the host to mimic DVFS behaviors in guests that
    are similar to real devices.
    
    Bug=b:284367589
    Test: tools/presubmit
    Test: Boot a VM on a pixel 8 with vcpufreq
    Change-Id: Ife9aec0161daa7852b87f982215926a618daf21c
    Signed-off-by: David Dai <davidai@google.com>

--- devices/src/virtcpufreq.rs
+++ devices/src/virtcpufreq.rs
@@ -22,18 +22,23 @@ use crate::Suspendable;
 
 const CPUFREQ_GOV_SCALE_FACTOR_DEFAULT: u32 = 100;
 const CPUFREQ_GOV_SCALE_FACTOR_SCHEDUTIL: u32 = 80;
+const SCHED_SCALE_CAPACITY: u32 = 1024;
 
 const SCHED_FLAG_RESET_ON_FORK: u64 = 0x1;
 const SCHED_FLAG_KEEP_POLICY: u64 = 0x08;
 const SCHED_FLAG_KEEP_PARAMS: u64 = 0x10;
 const SCHED_FLAG_UTIL_CLAMP_MIN: u64 = 0x20;
+const SCHED_FLAG_UTIL_CLAMP_MAX: u64 = 0x40;
 
 const SCHED_FLAG_KEEP_ALL: u64 = SCHED_FLAG_KEEP_POLICY | SCHED_FLAG_KEEP_PARAMS;
 
 #[derive(Serialize, Deserialize)]
 pub struct VirtCpufreq {
-    cpu_fmax: u32,
-    cpu_capacity: u32,
+    pcpu_fmax: u32,
+    pcpu_capacity: u32,
+    vcpu_fmax: u32,
+    vcpu_capacity: u32,
+    vcpu_relative_capacity: u32,
     pcpu: u32,
     util_factor: u32,
 }
@@ -78,13 +83,17 @@ fn get_cpu_util_factor(cpu_id: u32) -> Result<u32, Error> {
 
 impl VirtCpufreq {
     pub fn new(pcpu: u32, cpu_capacity: u32, cpu_fmax: u32) -> Self {
-        let cpu_capacity = get_cpu_capacity(pcpu).expect("Error reading capacity");
-        let cpu_fmax = get_cpu_maxfreq_khz(pcpu).expect("Error reading max freq");
         let util_factor = get_cpu_util_factor(pcpu).expect("Error getting util factor");
+        let pcpu_capacity = get_cpu_capacity(pcpu).expect("Error reading capacity");
+        let pcpu_fmax = get_cpu_maxfreq_khz(pcpu).expect("Error reading max freq");
+        let vcpu_relative_capacity = u32::try_from((u64::from(cpu_capacity) * u64::from(pcpu_fmax) / u64::from(cpu_fmax))).unwrap();
 
         VirtCpufreq {
-            cpu_fmax,
-            cpu_capacity,
+            pcpu_fmax,
+            pcpu_capacity,
+            vcpu_fmax: cpu_fmax,
+            vcpu_capacity: cpu_capacity,
+            vcpu_relative_capacity,
             pcpu,
             util_factor,
         }
@@ -111,7 +120,9 @@ impl BusDevice for VirtCpufreq {
         }
         // TODO(davidai): Evaluate opening file and re-reading the same fd.
         let freq = match get_cpu_curfreq_khz(self.pcpu) {
-            Ok(freq) => freq,
+            Ok(freq) => {
+                u32::try_from((u64::from(freq) * u64::from(self.pcpu_capacity) / u64::from(self.vcpu_relative_capacity))).unwrap()
+            },
             Err(e) => panic!("{}: Error reading freq: {}", self.debug_label(), e),
         };
 
@@ -133,13 +144,19 @@ impl BusDevice for VirtCpufreq {
         };
 
         // Util margin depends on the cpufreq governor on the host
-        let cpu_cap_scaled = self.cpu_capacity * self.util_factor / CPUFREQ_GOV_SCALE_FACTOR_DEFAULT;
-        let util = u64::from(cpu_cap_scaled) * u64::from(freq) / u64::from(self.cpu_fmax);
+        let cpu_cap_scaled = self.vcpu_capacity * self.util_factor / CPUFREQ_GOV_SCALE_FACTOR_DEFAULT;
+        let util = u32::try_from(u64::from(cpu_cap_scaled) * u64::from(freq) / u64::from(self.vcpu_fmax)).unwrap();
 
         let mut sched_attr = sched_attr::default();
         sched_attr.sched_flags =
-            SCHED_FLAG_KEEP_ALL | SCHED_FLAG_UTIL_CLAMP_MIN | SCHED_FLAG_RESET_ON_FORK;
-        sched_attr.sched_util_min = util.try_into().unwrap();
+            SCHED_FLAG_KEEP_ALL | SCHED_FLAG_UTIL_CLAMP_MIN | SCHED_FLAG_UTIL_CLAMP_MAX | SCHED_FLAG_RESET_ON_FORK;
+        sched_attr.sched_util_min = util;
+
+        if self.vcpu_fmax != self.pcpu_fmax {
+            sched_attr.sched_util_max = util;
+        } else {
+            sched_attr.sched_util_max = 1024;
+        }
 
         if let Err(e) = sched_setattr(0, &mut sched_attr, 0) {
             panic!("{}: Error setting util value: {}", self.debug_label(), e);

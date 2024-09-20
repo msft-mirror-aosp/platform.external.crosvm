google/crosvm

commit 7ddec973f562dacf3d4f4444efb44e2502321ef4

fix memory size when static swiotlb is enabled
Previously, if `--mem` was set to 100MB and `--swiotlb` to 1MB, then
we'd essentially allocate 101MB of RAM for the guest and tell it about a
single 101MB memory region in the FDT (adjacent regions are merged in
the FDT). In contrast, for hypervisors that don't require static
swiotlb, we'd only allocate 100MB of RAM (and then the guest would need
to choose 1MB of its own memory to dedicate to this purpose).

Now both cases will get the same RAM size.

Removed the swiotlb reference from memory_layout.md since it is part of
the RAM region from the perspective of the guest. The need for the
static swiotlb to be created as a separate memslot is more of a
hypervisor implementation detail I think.

BUG=b:346770542

Change-Id: I60b10521635c16fa77b4320de0e11f03ebeca0c5
Reviewed-on: https://chromium-review.googlesource.com/c/crosvm/crosvm/+/5637774
Reviewed-by: Elliot Berman <quic_eberman@quicinc.com>
Commit-Queue: Frederick Mayle <fmayle@google.com>
Reviewed-by: Richard Zhang <rizhang@google.com>

aarch64/src/lib.rs
@@ -172,10 +172,13 @@ fn get_bios_addr() -> GuestAddress {
 // Otherwise, returns None.
 fn get_swiotlb_addr(
     memory_size: u64,
+    swiotlb_size: u64,
     hypervisor: &(impl Hypervisor + ?Sized),
 ) -> Option<GuestAddress> {
     if hypervisor.check_capability(HypervisorCap::StaticSwiotlbAllocationRequired) {
-        Some(GuestAddress(AARCH64_PHYS_MEM_START + memory_size))
+        Some(GuestAddress(
+            AARCH64_PHYS_MEM_START + memory_size - swiotlb_size,
+        ))
     } else {
         None
     }
@@ -400,9 +403,18 @@ impl arch::LinuxArch for AArch64 {
         components: &VmComponents,
         hypervisor: &impl Hypervisor,
     ) -> std::result::Result<Vec<(GuestAddress, u64, MemoryRegionOptions)>, Self::Error> {
+        // Static swiotlb is allocated from the end of RAM as a separate memory region, so, if
+        // enabled, make the RAM memory region smaller to leave room for it.
+        let mut main_memory_size = components.memory_size;
+        if let Some(size) = components.swiotlb {
+            if hypervisor.check_capability(HypervisorCap::StaticSwiotlbAllocationRequired) {
+                main_memory_size -= size;
+            }
+        }
+
         let mut memory_regions = vec![(
             GuestAddress(AARCH64_PHYS_MEM_START),
-            components.memory_size,
+            main_memory_size,
             MemoryRegionOptions::new().align(get_block_size()),
         )];
 
@@ -416,7 +428,7 @@ impl arch::LinuxArch for AArch64 {
         }
 
         if let Some(size) = components.swiotlb {
-            if let Some(addr) = get_swiotlb_addr(components.memory_size, hypervisor) {
+            if let Some(addr) = get_swiotlb_addr(components.memory_size, size, hypervisor) {
                 memory_regions.push((
                     addr,
                     size,
@@ -803,7 +815,7 @@ impl arch::LinuxArch for AArch64 {
             psci_version,
             components.swiotlb.map(|size| {
                 (
-                    get_swiotlb_addr(components.memory_size, vm.get_hypervisor()),
+                    get_swiotlb_addr(components.memory_size, size, vm.get_hypervisor()),
                     size,
                 )
             }),

msft-mirror-aosp/platform.external.crosvm

commit a0db4972c4bd6ff6f5d6730448955071d4e9b7f1

fix fdt location when static swiotlb is enabled
In commit 7ddec973f562 ("fix memory size when static swiotlb is
enabled"), swiotlb moved so that the total memory size is equal to
components.memory_size. When fdt location is at the end of memory, the
swiotlb and fdt end up at overlapping location because the fdt address
computation did not account for the shrunken main memory size.

Add the main_memory_size adjustment into build_vm and adjust the
location of fdt_address when it is placed at the end of memory. I also
noticed the initrd_max_size also need to be adjusted.

Fixes: 7ddec973f562 ("fix memory size when static swiotlb is enabled")
Change-Id: Ic6ba46bdefdc99a0537342e09bb6dc3583438c63
Signed-off-by: Elliot Berman <quic_eberman@quicinc.com>

aarch64/src/lib.rs
@@ -363,6 +363,18 @@ fn get_vcpu_mpidr_aff<Vcpu: VcpuAArch64>(vcpus: &[Vcpu], index: usize) -> Option
     Some(vcpus.get(index)?.get_mpidr().ok()? & MPIDR_AFF_MASK)
 }
 
+fn main_memory_size(components: &VmComponents, hypervisor: &(impl Hypervisor + ?Sized)) -> u64 {
+    // Static swiotlb is allocated from the end of RAM as a separate memory region, so, if
+    // enabled, make the RAM memory region smaller to leave room for it.
+    let mut main_memory_size = components.memory_size;
+    if let Some(size) = components.swiotlb {
+        if hypervisor.check_capability(HypervisorCap::StaticSwiotlbAllocationRequired) {
+            main_memory_size -= size;
+        }
+    }
+    main_memory_size
+}
+
 impl arch::LinuxArch for AArch64 {
     type Error = Error;
 
@@ -372,14 +384,7 @@ impl arch::LinuxArch for AArch64 {
         components: &VmComponents,
         hypervisor: &impl Hypervisor,
     ) -> std::result::Result<Vec<(GuestAddress, u64, MemoryRegionOptions)>, Self::Error> {
-        // Static swiotlb is allocated from the end of RAM as a separate memory region, so, if
-        // enabled, make the RAM memory region smaller to leave room for it.
-        let mut main_memory_size = components.memory_size;
-        if let Some(size) = components.swiotlb {
-            if hypervisor.check_capability(HypervisorCap::StaticSwiotlbAllocationRequired) {
-                main_memory_size -= size;
-            }
-        }
+        let main_memory_size = main_memory_size(components, hypervisor);
 
         let mut memory_regions = vec![(
             GuestAddress(AARCH64_PHYS_MEM_START),
@@ -442,6 +447,8 @@ impl arch::LinuxArch for AArch64 {
         let has_bios = matches!(components.vm_image, VmImage::Bios(_));
         let mem = vm.get_memory().clone();
 
+        let main_memory_size = main_memory_size(&components, vm.get_hypervisor());
+
         let fdt_position = fdt_position.unwrap_or(if has_bios {
             FdtPosition::Start
         } else {
@@ -481,7 +488,7 @@ impl arch::LinuxArch for AArch64 {
                         let initrd_addr =
                             (kernel_end + (AARCH64_INITRD_ALIGN - 1)) & !(AARCH64_INITRD_ALIGN - 1);
                         let initrd_max_size =
-                            components.memory_size - (initrd_addr - AARCH64_PHYS_MEM_START);
+                            main_memory_size - (initrd_addr - AARCH64_PHYS_MEM_START);
                         let initrd_addr = GuestAddress(initrd_addr);
                         let initrd_size =
                             arch::load_image(&mem, &mut initrd_file, initrd_addr, initrd_max_size)
@@ -497,7 +504,7 @@ impl arch::LinuxArch for AArch64 {
             }
         };
 
-        let memory_end = GuestAddress(AARCH64_PHYS_MEM_START + components.memory_size);
+        let memory_end = GuestAddress(AARCH64_PHYS_MEM_START + main_memory_size);
 
         let fdt_address = match fdt_position {
             FdtPosition::Start => GuestAddress(AARCH64_PHYS_MEM_START),